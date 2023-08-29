8d0c5d532f4d834cfc8858d4cb1d5847d4cfe95e
SkepticMystic <rossk29@gmail.com>
2023-08-29 10:22:40 +0200

    feat: :sparkles: Add alignment prop on Toaster component

--- src/lib/components/Toaster.svelte
+++ src/lib/components/Toaster.svelte
@@ -1,62 +1,34 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import XMark from '$lib/components/icons/XMark.svelte';
 	import { toast } from '$lib/stores/toast.js';
-	import type { Toast } from '$lib/types/toast.js';
-	import { scale } from 'svelte/transition';
-	import TimerWheel from './TimerWheel.svelte';
+	import SingleToast from './SingleToast.svelte';
 
-	const alertTypes: Record<Toast['type'], string> = {
-		info: 'alert-info',
-		success: 'alert-success',
-		warning: 'alert-warning',
-		error: 'alert-error'
+	/** Align the toast along the x and y axes */
+	export let alignment: {
+		x?: 'start' | 'center' | 'end';
+		y?: 'top' | 'middle' | 'bottom';
+	} = {};
+
+	const resolvedAlignment = {
+		x: alignment.x ?? 'end',
+		y: alignment.y ?? 'bottom'
+	};
+
+	const toastAlignments = {
+		start: 'toast-start',
+		center: 'toast-center',
+		end: 'toast-end',
+		top: 'toast-top',
+		middle: 'toast-middle',
+		bottom: 'toast-bottom'
 	};
 </script>
 
-<div class="toast">
-	{#each $toast as { id, message, html, icon, type, duration_ms, showOnRoutes } (id)}
+<div class="toast {toastAlignments[resolvedAlignment.x]} {toastAlignments[resolvedAlignment.y]}">
+	{#each $toast as t (t.id)}
 		<!-- showOnRoutes -->
-		{#if !showOnRoutes?.length || showOnRoutes.some((r) => $page.url.pathname.startsWith(r))}
-			<div class="alert {alertTypes[type]} flex justify-between grow" out:scale={{ duration: 300 }}>
-				<div class="flex items-center gap-3">
-					<!-- icon -->
-					{#if typeof icon === 'string'}
-						<span>{icon}</span>
-					{:else}
-						<svelte:component this={icon} />
-					{/if}
-
-					<div>
-						<!-- html -->
-						{#if html}
-							{@html message}
-						{:else}
-							{message}
-						{/if}
-					</div>
-				</div>
-
-				<!-- duration_ms -->
-				{#if duration_ms}
-					<!-- Only set the timeout when the toast is rendered. This allows it to work with showOnRoutes -->
-					{@const _timer = setTimeout(() => toast.remove(id), duration_ms)}
-
-					<TimerWheel
-						{duration_ms}
-						on:click={() => {
-							toast.remove(id);
-							clearTimeout(_timer);
-						}}
-					>
-						<XMark w="w-4" h="h-4" />
-					</TimerWheel>
-				{:else}
-					<button class="btn btn-sm btn-circle btn-ghost" on:click={() => toast.remove(id)}>
-						<XMark w="w-4" h="h-4" />
-					</button>
-				{/if}
-			</div>
+		{#if !t.showOnRoutes?.length || t.showOnRoutes.some((r) => $page.url.pathname.startsWith(r))}
+			<SingleToast {t} />
 		{/if}
 	{/each}
 </div>
